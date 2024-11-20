f9b44d7008c9d053d37b2815655ab95dcd3667b9
Josh Marsh <theswampies.josh@gmail.com>
2024-11-20 19:27:29 +0000

    created basic home screen for game

--- src/components/home-screen.js
+++ src/components/home-screen.js
@@ -1,10 +1,54 @@
 import { Row, Col } from "antd";
+import { React, useState } from 'react';
+import { Input, InputNumber, Button } from 'antd';
+import '../styles/home-screen.css';
 
 function HomeScreen() {
+    const defaults = {
+        num_guesses: 3,
+        min_guesses: 1,
+        max_guesses: 10
+    }
+
+    const [totalGuesses, setTotalGuesses] = useState(defaults.num_guesses)
+    const [answer, setAnswer] = useState("")
+
+    const onClick = () => {
+        console.log('clicked');
+    };
+
     return (
-        <Row gutter={[8, 8]}>
-            <Col span={8} offset={8}>Col1</Col>
-        </Row>
+        <div>
+            <p>
+                remove debug text for guesses and answer before finishing!!!!
+            </p>
+
+            <div className="layout-row">
+                <Row gutter={[8, 8]}>
+                    <Col span={8} offset={8}>
+                        <p>enter word: {answer}</p> 
+                        <Input placeholder="enter word" defaultValue={""} onChange={(e) => setAnswer(e.target.value)}/>
+                    </Col>
+                </Row>
+            </div>
+
+            <div className="layout-row">
+                <Row gutter={[8, 8]}>
+                    <Col span={8} offset={8}>
+                        <p>number of guesses: {totalGuesses}</p> 
+                        <InputNumber min={defaults.min_guesses} max={defaults.max_guesses} defaultValue={defaults.num_guesses} onChange={(value) => setTotalGuesses(value)}/>
+                    </Col>
+                </Row>
+            </div>
+
+            <div className="layout-row">
+                <Row gutter={[8, 8]}>
+                    <Col span={8} offset={8}>
+                        <Button type="primary" onClick={onClick}>Start Game</Button>
+                    </Col>
+                </Row>
+            </div>
+        </div>
     );
     }
 
